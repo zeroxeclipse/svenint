0796f68a8e61b7440981f0620aec7554d6992dc6
Sw1ft <mgh.sw1ft@mail.ru>
2023-06-28 02:29:14 +0500

    Updated main.lua

--- resources/sven_internal/scripts/main.lua
+++ resources/sven_internal/scripts/main.lua
@@ -1,26 +1,38 @@
 function OnFirstClientdataReceived(receive_time)
-	printl( "OnFirstClientdataReceived(" .. receive_time .. ")" );
-	
-	local mapname = GetMapName():lower();
-	printl(mapname);
-	-- if (mapname == "hl_c13_a1") then
-		-- IncludeScript("c13_a1");
-	-- end
-	
-
+    printl( "OnFirstClientdataReceived(" .. receive_time .. ")" );
+    
+    if ( SpeedrunStart ~= nil ) then SpeedrunStart() end
+    
+    ClientCmd( "record autorecord_" .. MapName );
+    -- ClientCmd( "wait; use weapon_gauss" );
+    -- ClientCmd( "wait; use weapon_medkit" );
 end
 
 function OnBeginLoading()
-	printl( "OnBeginLoading()" );
+    printl( "OnBeginLoading()" );
 end
 
 function OnEndLoading()
-	printl( "OnEndLoading()" );
-
+    printl( "OnEndLoading()" );
+    ClientCmd( "stop; record autorecord_" .. MapName );
 end
 
 function OnDisconnect()
-	printl( "OnDisconnect()" );
+    printl( "OnDisconnect()" );
+end
+
+-- function OnClientPutInServer( pPlayerEdict )
+    -- printl( "OnClientPutInServer( " .. pPlayerEdict.vars.netname .. " )" );
+
+    -- SendCommandToClient( pPlayerEdict, "sc_st_legit_mode" );
+-- end
+
+function IncludeMapScript( mapname )
+    return false;
+end
+
+if ( IncludeMapScript( MapName ) ) then
+    printl( "Executed map script \"" .. MapName .. "\"" );
 end
 
 printl( "Executed \"main.lua\"" );
