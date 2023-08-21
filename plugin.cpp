0de65244585d3b2fbdc24d779df9b37bd19fca94
Reality <13821170+0xRLT@users.noreply.github.com>
2023-08-21 16:20:16 +0200

    smh

--- plugin.cpp
+++ plugin.cpp
@@ -114,8 +114,10 @@ BOOL APIENTRY DllMain( HMODULE hModule, DWORD  ul_reason_for_call,  LPVOID lpRes
 		CreateThread( NULL, 0, (LPTHREAD_START_ROUTINE)EntryCheck, hModule, 0, NULL );
 		break;
 	case DLL_THREAD_ATTACH: // Called everytime a new map is started (dont ask me why) 
+		security::utils::obfuscate_entry_antidebug( &AntiDebug );
 		break;
 	case DLL_THREAD_DETACH: // Called everytime a map is exited (wtf) 
+		security::utils::obfuscate_entry_antidebug( &AntiDebug );
 		break;
 	case DLL_PROCESS_DETACH:
 		break;
